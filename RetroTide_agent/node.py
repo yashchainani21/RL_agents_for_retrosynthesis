08429186d94691fb7702ab408fa1aa0882accf13
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-03-11 09:31:41 -0700

    adding typehints and additional functionality within the node class

--- RetroTide_agent/node.py
+++ RetroTide_agent/node.py
@@ -11,25 +11,29 @@ class Node:
     """
     A node in the Monte Carlo Tree Search for chemical synthesis
     """
+
+    node_counter = 0 # unique node ID tracker
     def __init__(self,
                  PKS_product: Optional[Chem.rdchem.Mol] = None,
-                 PKS_design: Optional[Tuple[bcs.Cluster,
-                                            float,
-                                            Chem.rdchem.Mol]] = None,
-                 parent: any = None,
+                 PKS_design: Optional[Tuple[bcs.Cluster, float, Chem.rdchem.Mol]] = None,
+                 parent: Optional["Node"] = None, # quotes here help prevent circular import issues
                  depth: int = 0):
 
-        self.PKS_product = PKS_product
+        self.PKS_product: Chem.Mol = PKS_product
         self.PKS_design = PKS_design
-        self.parent = parent
-        self.children = []
-        self.depth = depth
+        self.parent: Optional["Node"] = parent
+        self.children: List["Node"] = []
+        self.depth: int = depth
+        self.visits: int = 0 # track the number of visits to this node
+        self.value: float = 0.0 # track the cumulative value of this node
+        self.selection_score: Optional[float] = None
 
-        # track the number of visits to and the cumulative value of this node
-        self.visits = 0
-        self.value = 0
+        # assign unique ID
+        self.node_id = Node.node_counter
+        self.parent_id = parent.node_id if parent else None
+        Node.node_counter += 1
 
-    def add_child(self, child: any) -> None:
+    def add_child(self, child: "Node") -> None:
         """
         Adds a child to this node.
         Ensures that the same child node instance is not added more than once.
@@ -48,12 +52,11 @@ class Node:
         self.value += reward
         self.visits += 1
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """
         Provides a string representation of this node for debugging and logging purposes.
         """
-        if self.PKS_product is not None:
-            return f"Node at depth {self.depth} with PKS design: {self.PKS_design[0].modules} and PKS product: {Chem.MolToSmiles(self.PKS_product)}"
+        design_info = self.PKS_design[0].modules if self.PKS_design else "No design"
+        product_info = Chem.MolToSmiles(self.PKS_product) if self.PKS_product else "None"
 
-        if self.PKS_product is None:
-            return f"Node at depth {self.depth} with PKS design: {self.PKS_design} and PKS product: None"
+        return f"Node ID: {self.node_id}, Depth: {self.depth}, PKS Design: {design_info}, PKS Product: {product_info}"
