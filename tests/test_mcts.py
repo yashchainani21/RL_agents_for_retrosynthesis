41aae8a685ea3b2792b1c5bfae288bcde5c2f320
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-03-11 09:48:26 -0700

    remove explore_least_visited_child policy

--- tests/test_mcts.py
+++ tests/test_mcts.py
@@ -2,45 +2,6 @@ from rdkit import Chem
 from RetroTide_agent.node import Node
 from RetroTide_agent.mcts import MCTS
 
-def test_explore_least_visited_selection_policy_returns_least_visited_child():
-    """
-    Test whether the 'explore_least_visited' selection policy correctly prioritizes child node with the least visits.
-    In this scenario, two child nodes are initialized with different number of visits.
-    Each child node is added as a child node to the parent, root node with the .add_child method.
-    This test checks if 'explore_least_visited' selection policy correctly identifies and selects less visited child.
-    This type of selection policy inherently biases exploration over exploitation.
-    """
-    root = Node(PKS_product = None,
-                PKS_design = None,
-                depth = 0)
-
-    child1 = Node(PKS_product = Chem.MolFromSmiles("CC"),
-                  PKS_design = None,
-                  depth = 1)
-
-    child2 = Node(PKS_product = Chem.MolFromSmiles("CCCC"),
-                  PKS_design = None,
-                  depth = 1)
-
-    # manually set visits to simulate some kind of prior exploration
-    child1.visits = 5
-    child2.visits = 3
-
-    root.add_child(child1)
-    root.add_child(child2)
-
-    # initialize an mcts object
-    mcts = MCTS(root = root,
-                target_molecule = Chem.MolFromSmiles("CCCCCCCC"),
-                max_depth = 10,
-                maxPKSDesignsRetroTide = 25,
-                selection_policy = "explore_least_visited")
-
-    selected_node = mcts.select(root)
-
-    # we expect child2 to be selected under this selection policy since it has fewer visits
-    assert selected_node == child2
-
 def test_UCB1_selection_policy():
     """
     Test whether the UCB1 selection policy correctly prioritizes child node with the highest value from UCB1 formula.
