3e86e3a65d454bb1e97435501222100ff8de2c2a
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-03-18 11:55:27 +0800

    separete the generation of PKS products and their offloading into two separate workflows, i.e., scripts

--- scripts/01_generate_bound_PKS_products.py
+++ scripts/01_generate_bound_PKS_products.py
@@ -11,34 +11,6 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from collections import OrderedDict
 
-def run_pks_release_reaction(pks_release_mechanism: str,
-                             bound_product_mol: Chem.Mol) -> Chem.Mol:
-    """
-    Run an offloading reaction to release a bound PKS product.
-    Two types of offloading reactions are currently supported: thiolysis and cyclization.
-    A thiolysis offloading reaction will result in the formation of a carboxylic acid.
-    Meanwhile, a cyclization offloading reaction results in the formation of a lactone ring.
-    """
-
-    if pks_release_mechanism == 'thiolysis':
-        Chem.SanitizeMol(bound_product_mol)  # run detachment reaction to produce terminal acid group
-        rxn = AllChem.ReactionFromSmarts('[C:1](=[O:2])[S:3]>>[C:1](=[O:2])[O].[S:3]')
-        unbound_product_mol = rxn.RunReactants((bound_product_mol,))[0][0]
-        Chem.SanitizeMol(unbound_product_mol)
-        return unbound_product_mol
-
-    if pks_release_mechanism == 'cyclization':
-        Chem.SanitizeMol(bound_product_mol)  # run detachment reaction to cyclize bound substrate
-        rxn = AllChem.ReactionFromSmarts('([C:1](=[O:2])[S:3].[O,N:4][C:5][C:6])>>[C:1](=[O:2])[*:4][C:5][C:6].[S:3]')
-        try:
-            unbound_product_mol = rxn.RunReactants((bound_product_mol,))[0][0]
-            Chem.SanitizeMol(unbound_product_mol)
-            return unbound_product_mol
-
-        # if the bound substrate cannot be cyclized, then return None
-        except:
-            raise ValueError("\nUnable to perform cyclization reaction")
-
 all_starters_list = list(bcs.starters.keys())
 all_extenders_list = list(bcs.extenders.keys())
 
@@ -67,12 +39,16 @@ for starter in all_starters_list:
     unbound_LM_acid_product = run_pks_release_reaction(pks_release_mechanism = 'thiolysis',
                                                        bound_product_mol = bound_LM_product)
 
+    unbound_LM_acid_product = Chem.MolToSmiles(unbound_LM_acid_product)
+
     all_PKS_designs_and_products_dict.update({loading_module: unbound_LM_acid_product})
 
     # now try releasing the bound loading module product with a cyclization reaction
     try:
         unbound_LM_cyclic_product = run_pks_release_reaction(pks_release_mechanism = 'cyclization',
                                                              bound_product_mol = bound_LM_product)
+
+        unbound_LM_cyclic_product = Chem.MolToSmiles(unbound_LM_cyclic_product)
 
         # if cyclization is possible, store the resulting lactone
         all_PKS_designs_and_products_dict.update({loading_module: unbound_LM_cyclic_product})
---
+++ scripts/02_generate_unbound_PKS_products.py
@@ -0,0 +1,29 @@
+from rdkit import Chem
+
+def run_pks_release_reaction(pks_release_mechanism: str,
+                             bound_product_mol: Chem.Mol) -> Chem.Mol:
+    """
+    Run an offloading reaction to release a bound PKS product.
+    Two types of offloading reactions are currently supported: thiolysis and cyclization.
+    A thiolysis offloading reaction will result in the formation of a carboxylic acid.
+    Meanwhile, a cyclization offloading reaction results in the formation of a lactone ring.
+    """
+
+    if pks_release_mechanism == 'thiolysis':
+        Chem.SanitizeMol(bound_product_mol)  # run detachment reaction to produce terminal acid group
+        rxn = AllChem.ReactionFromSmarts('[C:1](=[O:2])[S:3]>>[C:1](=[O:2])[O].[S:3]')
+        unbound_product_mol = rxn.RunReactants((bound_product_mol,))[0][0]
+        Chem.SanitizeMol(unbound_product_mol)
+        return unbound_product_mol
+
+    if pks_release_mechanism == 'cyclization':
+        Chem.SanitizeMol(bound_product_mol)  # run detachment reaction to cyclize bound substrate
+        rxn = AllChem.ReactionFromSmarts('([C:1](=[O:2])[S:3].[O,N:4][C:5][C:6])>>[C:1](=[O:2])[*:4][C:5][C:6].[S:3]')
+        try:
+            unbound_product_mol = rxn.RunReactants((bound_product_mol,))[0][0]
+            Chem.SanitizeMol(unbound_product_mol)
+            return unbound_product_mol
+
+        # if the bound substrate cannot be cyclized, then return None
+        except:
+            raise ValueError("\nUnable to perform cyclization reaction")
