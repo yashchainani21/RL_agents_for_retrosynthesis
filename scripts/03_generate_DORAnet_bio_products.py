fbdc0002a7dc89ef692a8d182ff5b69e4cc92f1d
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-04-14 17:38:21 -0500

    update enzymatically modifying PKS products with parallelization

--- scripts/03_generate_DORAnet_bio_products.py
+++ scripts/03_generate_DORAnet_bio_products.py
@@ -8,35 +8,37 @@ cofactors_df = pd.read_csv('../data/raw/all_cofactors.csv')
 cofactors_list = [Chem.MolToSmiles(Chem.MolFromSmiles(smi)) for smi in cofactors_df["SMILES"]]
 
 # Define file paths based on modification type
-max_module = "LM"
+input_products = "LM" # choose from "LM", "M1", "M2", "M3", "BIO1", "CHEM1"
+output_products = "BIO1" # choose from either "BIO1", or "BIO2"
+module = "LM" # choose from "LM", "M1", "M2", "M3"
 modify_PKS_products = True
 DORAnet_product_type_to_modify = None
 
-if modify_PKS_products:
-    precursors_filepath = f'../data/interim/unique_PKS_products_no_stereo_{max_module}.txt'
-    output_filepath = f'../data/interim/DORAnet_BIO1_products_from_{max_module}_polyketides.txt'
+# set input and output filepaths as None first and modify later
+output_filepath = None
+precursors_filepath = None
 
-if DORAnet_product_type_to_modify == 'BIO':
-    precursors_filepath = f'../data/interim/DORAnet_BIO1_products_from_{max_module}_polyketides.txt'
-    output_filepath = f'../data/interim/DORAnet_BIO2_products_from_{max_module}_polyketides.txt'
+# if performing DORAnet modifications on polyketides
+if input_products in ("LM", "M1", "M2", "M3"):
+    precursors_filepath = f'../data/interim/unique_PKS_products_no_stereo_{input_products}.txt'
+    output_filepath = f'../data/interim/DORAnet_{output_products}_from_{input_products}.txt'
 
-if DORAnet_product_type_to_modify == 'CHEM':
-    precursors_filepath = f'../data/interim/DORAnet_CHEM1_products_from_{max_module}_polyketides.txt'
-    output_filepath = f'../data/interim/DORAnet_CHEM2_products_from_BIO1.txt'
+# if performing DORAnet modifications for a second-step on DORAnet products from the first step
+if input_products in ("BIO1", "CHEM1"):
+    precursors_filepath = f'../data/interim/DORAnet_{input_products}_from_{module}.txt'
+    output_filepath = f'../data/interim/DORAnet_{output_products}_from_{input_products}_from{module}.txt'
 
 # Read precursor SMILES
 with open(precursors_filepath, 'r') as precursors_file:
     precursors_list = [s.strip('\n') for s in precursors_file.readlines()]
 
-
 def perform_DORAnet_bio_1step(precursor_smiles: str):
     """Generates one-step DORAnet products for a given precursor SMILES string."""
     forward_network = enzymatic.generate_network(
-        job_name=precursor_smiles,
-        starters={precursor_smiles},
-        gen=1,
-        direction="forward"
-    )
+        job_name = precursor_smiles,
+        starters = {precursor_smiles},
+        gen = 1,
+        direction = "forward")
 
     generated_bioproducts_list = []
 
@@ -49,7 +51,6 @@ def perform_DORAnet_bio_1step(precursor_smiles: str):
 
     return generated_bioproducts_list
 
-
 def process_precursors_parallel(precursors, num_workers=mp.cpu_count()):
     """Parallelize the processing of precursor SMILES using multiprocessing."""
     with mp.Pool(processes=num_workers) as pool:
@@ -60,14 +61,13 @@ def process_precursors_parallel(precursors, num_workers=mp.cpu_count()):
 
     return all_bioproducts_list
 
-
 if __name__ == "__main__":
-    # Run in parallel
+
     print(f"Running with {mp.cpu_count()} cores...")
-    all_bioproducts_list = process_precursors_parallel(precursors_list, num_workers=mp.cpu_count())
+    all_bioproducts_list = process_precursors_parallel(precursors_list, num_workers = mp.cpu_count())
+    all_bioproducts_list = list(set(all_bioproducts_list))
+    print(f"\nNumber of total, unique bioproducts generated: {len(all_bioproducts_list)}\n")
 
-    print(f"\nNumber of total bioproducts generated: {len(all_bioproducts_list)}\n")
-
-    # Save results
+    # save all biologically modified products
     with open(output_filepath, 'w') as output_file:
         output_file.write('\n'.join(all_bioproducts_list))
