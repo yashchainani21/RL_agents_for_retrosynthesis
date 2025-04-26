21a95e342f69f8bd83104e9af39aad6bf9e70ea2
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-04-26 10:54:48 -0500

    update tests

--- tests/test_rxn_mapping_data.py
+++ tests/test_rxn_mapping_data.py
@@ -35,7 +35,9 @@ def test_if_biochem_mappings_are_unique():
     all_mapped_bio_and_chem_rxns = pd.read_csv("../data/processed/all_unique_bio_and_chem_mapped_rxns.csv")
     assert sum(all_mapped_bio_and_chem_rxns.duplicated()) == 0
 
-def test_if_reactant_template_pair_datasets_are_unique():
+def test_if_reactant_template_pair_dataset_has_only_unique_entries():
     """Ensure all mapped reaction-template pairs arising from the above datasets are unique."""
     all_reactant_template_pairs = pd.read_csv("../data/processed/all_bio_and_chem_unique_reactant_template_pairs_no_stereo.csv")
     assert sum(all_reactant_template_pairs.duplicated()) == 0
+
+def test_if_reactant_template
