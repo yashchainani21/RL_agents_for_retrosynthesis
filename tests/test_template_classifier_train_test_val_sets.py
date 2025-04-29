8ac52bffcc62abaeac3aa1999d6158477fb44689
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-04-29 14:05:36 -0500

    update test docstring

--- tests/test_template_classifier_train_test_val_sets.py
+++ tests/test_template_classifier_train_test_val_sets.py
@@ -2,31 +2,49 @@ import pytest
 import pandas as pd
 
 def test_no_missing_values_in_reactant_template_dataset_before_splits():
-    """Check that there are no NaN values in the reactant-template dataset before splits."""
+    """
+    Check that there are no NaN values in the reactant-template dataset before splits.
+    """
     reactant_template_pairs_before_splits_df = pd.read_csv('../data/processed/all_bio_and_chem_unique_reactant_template_pairs_no_stereo_w_integer_labels.csv')
     assert reactant_template_pairs_before_splits_df.isnull().values.any() == False
 
 def test_no_missing_values_in_reactant_template_training_dataset_after_splits():
-    """Check that there are no NaN values in the reactant-template training dataset after splits."""
+    """
+    Check that there are no NaN values in the reactant-template training dataset after splits.
+    """
     reactant_template_training_pairs_df = pd.read_csv('../data/training/training_reactant_template_pairs.csv')
     assert reactant_template_training_pairs_df.isnull().values.any() == False
 
 def test_no_missing_values_in_reactant_template_testing_dataset_after_splits():
-    """Check that there are no NaN values in the reactant-template testing dataset after splits."""
+    """
+    Check that there are no NaN values in the reactant-template testing dataset after splits.
+    """
     reactant_template_testing_pairs_df = pd.read_csv('../data/testing/testing_reactant_template_pairs.csv')
     assert reactant_template_testing_pairs_df.isnull().values.any() == False
 
 def test_no_missing_values_in_reactant_template_validation_dataset_after_splits():
-    """Check that there are no NaN values in the reactant-template validation dataset after splits."""
+    """
+    Check that there are no NaN values in the reactant-template validation dataset after splits.
+    """
     reactant_template_validation_template_pairs_df = pd.read_csv('../data/validation/validation_reactant_template_pairs.csv')
     assert reactant_template_validation_template_pairs_df.isnull().values.any() == False
 
 def test_each_template_in_reactant_template_dataset_has_minimally_10_examples_before_splits():
+    """
+    Ensure there are no templates with less than 10 reactants in the dataset of unique reactant-template pairs.
+    This is because having at least 10 examples per template enables an 80/10/10 split by stratifying by template.
+    """
     reactant_template_pairs_before_splits_df = pd.read_csv('../data/processed/all_bio_and_chem_unique_reactant_template_pairs_no_stereo_w_integer_labels.csv')
     template_frequency_counts = reactant_template_pairs_before_splits_df['Template Label'].value_counts()
 
     for idx, count in enumerate(template_frequency_counts):
         assert count >= 10
+
+def test_if_every_template_appears_at_least_once_in_each_set():
+    reactant_template_training_pairs_df = pd.read_csv('../data/training/training_reactant_template_pairs.csv')
+    reactant_template_testing_pairs_df = pd.read_csv('../data/testing/testing_reactant_template_pairs.csv')
+    reactant_template_validation_template_pairs_df = pd.read_csv('../data/validation/validation_reactant_template_pairs.csv')
+
 
 def test_no_overlap_between_train_test_val_split_of_reactant_template_pairs():
     """
