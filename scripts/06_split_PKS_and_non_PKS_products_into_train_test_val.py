f54dbf887cae055d32bef0497da50654bf8e988e
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-04-30 10:23:37 -0500

    rename scripts

--- scripts/06_split_PKS_and_non_PKS_products_into_train_test_val.py
+++ scripts/06_split_PKS_and_non_PKS_products_into_train_test_val.py
@@ -1,8 +1,12 @@
 """
-In this script, we featurize previously combined PKS and non-PKS products.
+In this script, we split previously combined PKS and non-PKS products into train/test/validation sets.
+Splits will be stratified by the label for each molecule which determines if a product is a polyketide (1) or not (0).
+This ensures that the distribution of polyketides to non-polyketides is retained across all sets.
 """
 
-import pandas as pd
+import dask.dataframe as dd
 
 module = "LM"
-outfile_path = f'../data/processed/{module}_labeled_products.parquet'
+input_PKS_and_non_PKS_products_path = f'../data/processed/{module}_labeled_products.parquet'
+
+PKS_and_non_PKS_products_df = dd.read_parquet(input_PKS_and_non_PKS_products_path)
