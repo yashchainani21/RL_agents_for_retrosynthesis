71ef7ed26f23efeb4f6e0f7bee1634333fccc0ac
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-05-03 16:59:15 -0500

    downsample non-PKS products

--- scripts/07_featurize_PKS_and_non_PKS_products_with_ecfp4_with_downsampling.py
+++ scripts/07_featurize_PKS_and_non_PKS_products_with_ecfp4_with_downsampling.py
@@ -32,7 +32,12 @@ if rank == 0:
     PKS_products_df = all_products_df[all_products_df["labels"] == 1]
     non_PKS_products_df = all_products_df[all_products_df["labels"] == 0]
 
+    # downsample the set of non-PKS products to the pre-determine ratio of non-PKS to PKS products
+    num_non_PKS_products_to_sample = non_PKS_products_df.shape[0]**ratio_of_non_PKS_to_PKS_products
     non_PKS_products_df_index = non_PKS_products_df.index.values
+    subsampled_indices = np.random.choice(non_PKS_products_df_index,
+                                          size = num_non_PKS_products_to_sample,
+                                          replace = False)
 
     smiles_list = data['SMILES'].tolist()
     label_list = data['labels'].tolist()
