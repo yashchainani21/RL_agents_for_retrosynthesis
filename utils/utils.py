4f9df8889dc27c7896c92dc15b61408f77e514c2
yashchainani21 <ychainani21@gmail.com>
2025-12-26 09:13:02 -0600

    add are_isomorphic function

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,9 @@
 from rdkit import Chem
+
+def are_isomorphic(mol1: Chem.Mol, mol2: Chem.Mol, consider_stereo: bool = False):
+    if consider_stereo:
+        is_isomorphic = mol1.HasSubstructMatch(mol2, useChirality = True) and mol2.HasSubstructMatch(mol1, useChirality = True)
+    else:
+        is_isomorphic = mol1.HasSubstructMatch(mol2) and mol2.HasSubstructMatch(mol1)
+        
+    return is_isomorphic
